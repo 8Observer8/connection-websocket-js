ea85cdd1c06db707e273eb1a4f0a50db37812a36
Ivan Enzhaev <8observer8@gmail.com>
2020-12-28 03:02:13 +0400

    Output info to window instead of console

--- src/client/main.js
+++ src/client/main.js
@@ -4,9 +4,14 @@ function main()
     // const socket = new WebSocket("ws://localhost:3000");
     const socket = new WebSocket("wss://connection-websocket-js.herokuapp.com");
 
+    const output = document.getElementById("output");
+    output.innerHTML = "Please, wait for connection..."
+
     socket.onopen = () =>
     {
-        console.log("Client was connected to server");
+        const message = "Client was connected to server";
+        console.log(message);
+        output.innerHTML = message;
     };
 }
 
